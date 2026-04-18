80eb4ab314761027c8672d2d4fe6b3ea2a1a4654
Achintya Gupta <achintya.mann@gmail.com>
2026-04-19 01:33:44 +0530

    Handled class imbalance using SMOTE

--- src/train.py
+++ src/train.py
@@ -45,6 +45,12 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
+from imblearn.over_sampling import SMOTE
+
+smote = SMOTE(random_state=42)
+X_train, y_train = smote.fit_resample(X_train, y_train)
+
+print("After SMOTE:", np.bincount(y_train))
 # ----------------------------
 # BASELINE MODEL
 # ----------------------------
